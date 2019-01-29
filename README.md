5d153e5d29312e482b80bf43dcef074f042b31db
Ian Laue <laueian@gmail.com>
2019-01-29 15:44:05 -0800

    test graphql code with ORM

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # award-me
+
 Scholarship and academic awards application pipeline for students and administrators.
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHTTP = require('express-graphql')
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const schema = require('./schema')
-app.use('/graphql', graphqlHTTP({
+const config = require("./config/db");
+mongoose.connect(
+  config.connectionString,
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
+
+const schema = require("./graphql/schema");
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}))
+  })
+);
 
-app.use('', (req,res) => {
-    res.send('Hello')
-})
-
+app.use("", (req, res) => {
+  res.send("Hello");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
